8287219bc579b742d84867f5d50ca60a5e712e92
Steve Purcell <steve@sanityinc.com>
2019-08-12 11:06:29 +1200

    More layers but less magic

--- keyboards/keebio/iris/keymaps/sanityinc/config.h
+++ keyboards/keebio/iris/keymaps/sanityinc/config.h
@@ -28,6 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_VAL_STEP 8
 #define RGBLIGHT_LIMIT_VAL 200 // down from 255
 
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TOGGLE 2
+
 #define RGBLIGHT_SPLIT
 #define RGBLIGHT_SLEEP
 #define MOUSEKEY_MAX_SPEED 20
